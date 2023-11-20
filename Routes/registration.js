c9c45d70cea0f415dc6e48a8a73d5debc63b7c88
Samuelix96 <131618047+Samuelix96@users.noreply.github.com>
2023-11-20 16:14:05 +0100

    modify registration

--- Routes/registration.js
+++ Routes/registration.js
@@ -1,77 +1,76 @@
-const express = require("express")
-const  registration = express.Router();
-const bcrypt = require("bcrypt");
-const UsersModel = require("../Models/users");
+/** @format */
+
+const express = require('express');
+const registration = express.Router();
+const bcrypt = require('bcrypt');
+const UsersModel = require('../Models/users');
 const jwt = require('jsonwebtoken');
-require("dotenv").config();
-const multer = require("multer")
-const crypto = require("crypto")
-const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage} = require("multer-storage-cloudinary");
+require('dotenv').config();
+const multer = require('multer');
+const crypto = require('crypto');
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key : process.env.CLOUDINARY_APY_KEY,
-  api_secret : process.env.CLOUDINARY_APY_SECRET,
-})
+  api_key: process.env.CLOUDINARY_APY_KEY,
+  api_secret: process.env.CLOUDINARY_APY_SECRET,
+});
 
 const cloudStorage = new CloudinaryStorage({
   cloudinary: cloudinary,
-  params : {
-      folder: "UsersImages",
-      format: async(req,file) => "png",
-      public_id : (req, file) => file.name
-  }
-})
+  params: {
+    folder: 'UsersImages',
+    format: async (req, file) => 'png',
+    public_id: (req, file) => file.name,
+  },
+});
 
-const cloudUpload = multer({ storage: cloudStorage})
+const cloudUpload = multer({ storage: cloudStorage });
 
-registration.post('/registration/cloudUpload', cloudUpload.single("avatar"), async(req, res) => {
-  try {
-      res.status(200).json({avatar: req.file.path})
-  } catch (error) {
+registration.post(
+  '/registration/cloudUpload',
+  cloudUpload.single('avatar'),
+  async (req, res) => {
+    try {
+      res.status(200).json({ avatar: req.file.path });
+    } catch (error) {
       res.status(500).send({
-          statusCode: 500,
-          message: "Error internal server"
-      })
+        statusCode: 500,
+        message: 'Error internal server',
+      });
+    }
   }
-})
+);
 
-
-
-registration.post('/registration', async(req,res) => {
-     
+registration.post('/registration', async (req, res) => {
   const existingUser = await UsersModel.findOne({ email: req.body.email });
 
   if (existingUser) {
     res.status(500).send({
-      message: "Error internal server",
-      error: error.message,
-      statusCode: 500
-  })
+      message: 'Error internal server',
+
+      statusCode: 500,
+    });
   }
 
   try {
-   
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-   
     const newUser = new UsersModel({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
-      nickName : req.body.nickName,
+      nickName: req.body.nickName,
       email: req.body.email,
       password: hashPassword,
       birth: req.body.birth,
       avatar: req.body.avatar,
-      role:req.body.role
+      role: req.body.role,
     });
 
-   
     const addUser = await newUser.save();
 
-   
     const token = jwt.sign(
       {
         id: addUser._id,
@@ -93,11 +92,11 @@ registration.post('/registration', async(req,res) => {
     });
   } catch (error) {
     res.status(500).send({
-        message: "Errore nel server interno",
-        error: error.message,
-        statusCode: 500
-    })
+      message: 'Errore nel server interno',
+      error: error.message,
+      statusCode: 500,
+    });
   }
-})
+});
 
-module.exports= registration;
+module.exports = registration;
